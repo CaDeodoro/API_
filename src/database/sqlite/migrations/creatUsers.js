83c8b5dc9763b7c47781a3943a6945899b894204
Rafael Veiga <rafaelveig@hotmail.com>
2023-08-31 22:27:14 -0300

    commit do amor

--- src/database/sqlite/migrations/creatUsers.js
+++ src/database/sqlite/migrations/creatUsers.js
@@ -1,5 +1,5 @@
 const creatUsers = ` 
-  CREAT TABLE IF NOT EXISTS users (
+  CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR,
   email VARCHAR,
@@ -8,6 +8,6 @@ const creatUsers = `
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
-`;
+`
 
-module.exports = creatUsers;
+module.exports = creatUsers
